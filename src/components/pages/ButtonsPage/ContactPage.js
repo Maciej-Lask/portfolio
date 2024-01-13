7214fe54e46e4e20ab406dce0b917f988f436292
Maciej Laskowski <maciek30088@gmail.com>
2024-01-13 15:41:47 +0100

    Finish contact page cards

--- src/components/pages/ButtonsPage/ContactPage.js
+++ src/components/pages/ButtonsPage/ContactPage.js
@@ -1,36 +1,77 @@
 import styles from './ContactPage.module.scss';
-import { Row, Col, Container } from 'react-bootstrap';
+import { Row, Col, Container, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
+const variant = 'dark';
+const borderColor = 'light';
 const ContactPage = () => (
   <div className={styles.contactPage}>
     <Container>
       <h1 className="text-center m-0 p-3">My contact info</h1>
       <Row>
-        <Col>
-          <h2>Phone</h2>
-          <FontAwesomeIcon className={styles.links} icon={faPhone} />
-          <p>+48 510 706 003</p>
-        </Col>
-        <Col>
-          <h2>Email</h2>
-          <FontAwesomeIcon className={styles.links} icon={faEnvelope} />
-          <p>maciek30088@gmail.com</p>
-        </Col>
-        <Col>
-          <h2>My socials</h2>
-          <a className={styles.links} href="https://github.com/Maciej-Lask">
-            <FontAwesomeIcon icon={faGithub} />
-          </a>
-          <a
-            className={styles.links}
-            href="https://www.linkedin.com/in/maciej-lask/"
+        <Col className="p-0">
+          <Card
+            className={styles.card}
+            border={borderColor}
+            bg={variant}
+            key={variant}
+            text="white"
           >
-            <FontAwesomeIcon icon={faLinkedin} />
-          </a>
+            <Card.Header>Phone</Card.Header>
+            <Card.Body>
+              <FontAwesomeIcon className={`${styles.links}`} icon={faPhone} />
+              <Card.Text>+48 510 706 003</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col className="p-0">
+          <Card
+            className={styles.card}
+            border={borderColor}
+            bg={variant}
+            key={variant}
+            text="white"
+          >
+            <Card.Header>Email</Card.Header>
+            <Card.Body>
+              <FontAwesomeIcon className={styles.links} icon={faEnvelope} />
+              <Card.Text>maciek30088@gmail.com</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col className="p-0">
+          <Card
+            className={styles.card}
+            border={borderColor}
+            bg={variant}
+            key={variant}
+            text="white"
+          >
+            <Card.Header>My socials</Card.Header>
+            <Card.Body>
+              <Card.Text>
+                Github:
+                <Card.Link
+                  className={styles.links}
+                  href="https://github.com/Maciej-Lask"
+                >
+                  <FontAwesomeIcon icon={faGithub} />
+                </Card.Link>
+              </Card.Text>
+              <Card.Text>
+                LinkedIn:
+                <Card.Link
+                  className={styles.links}
+                  href="https://www.linkedin.com/in/maciej-lask/"
+                >
+                  <FontAwesomeIcon icon={faLinkedin} />
+                </Card.Link>
+              </Card.Text>
+            </Card.Body>
+          </Card>
         </Col>
       </Row>
     </Container>
--- src/components/pages/Home/HomePage.js
+++ src/components/pages/Home/HomePage.js
@@ -1,18 +1,77 @@
+// home.js
 
-import { Container } from 'react-bootstrap';
+import React from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 import styles from './HomePage.module.scss';
 
 const HomePage = () => (
   <div className={styles.homePage}>
     <Container>
+      <Row>
+        <header>
+          <h1 className="text-center m-0 p-3">Hi, I'am Maciej</h1>
+          <p>Junior Full Stack Developer</p>
+        </header>
+      </Row>
 
-    <h1 className="text-center m-0 p-3">Home Page</h1>
-    <p>This is the Home Page</p>
-    <p>And this is some more text</p>
-    <p>And this is even more text</p>
-    <p>And this is even more text</p>
-    <p>And this is even more text</p>
+      <Row className="mt-4">
+        <Col>
+          <Card>
+            <Card.Body>
+              <h2>About Me</h2>
+              <p>
+                I am an ambitious Junior Full Stack Developer with a passion for
+                creating innovative solutions. My driving force is a constant
+                desire for growth and acquiring new skills. My projects are
+                characterized by a focus on code quality and meeting user needs.
+                Currently I am learning Angular and TailwindCSS.
+              </p>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
 
+      <Row className="mt-4">
+        <Col className='col-2'>
+          <Card>
+            <Card.Body>
+              <h2>Skills</h2>
+              <p>
+                HTML | CSS | JavaScript | Node.js | TypeScript | React | Redux |
+                Bootstrap | Express | WebSocket | MongoDB | Mongoose | GitHub |
+                Jira | MySQL | RWD | NPM | Webpack | NestJS | Prisma | AJAX
+              </p>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            <Card.Body>
+              <h2>My Projects</h2>
+              <Card>
+                <Card.Body>
+                  <h3>Project 1</h3>
+                  <p>Description of project 1.</p>
+                </Card.Body>
+              </Card>
+              <Card>
+                <Card.Body>
+                  <h3>Project 2</h3>
+                  <p>Description of project 2.</p>
+                </Card.Body>
+              </Card>
+              <Card>
+                <Card.Body>
+                  <h3>Project 3</h3>
+                  <p>Description of project 3.</p>
+                </Card.Body>
+              </Card>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+
+      {/* Add more sections for Projects, Professional Experience, Contact, etc. */}
     </Container>
   </div>
 );
